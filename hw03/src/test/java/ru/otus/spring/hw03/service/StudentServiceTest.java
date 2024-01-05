b4f4d049fd62575cc22859dba80eb82e0928490b
Farida <Purumok6!>
2024-01-05 15:24:49 +0300

    hw04 возврат недочетов обратно, чтобы нормально выглядел PR hw03<-hw04

--- hw03/src/test/java/ru/otus/spring/hw03/service/StudentServiceTest.java
+++ hw03/src/test/java/ru/otus/spring/hw03/service/StudentServiceTest.java
@@ -3,30 +3,44 @@ package ru.otus.spring.hw03.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import ru.otus.spring.hw03.domain.Student;
 
-import java.util.stream.Stream;
-
 @DisplayName("Unit тест корректности создания объекта Student в классе StudentServiceImpl")
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StudentServiceTest {
-    private static LocalizedIOService ioService;
-    private static StudentService studentService;
+    private LocalizedIOService ioService;
+    private StudentService studentService;
 
     @BeforeAll
-    public static void init() {
-        ioService = Mockito.mock(LocalizedIOService.class);
-        studentService = new StudentServiceImpl(ioService);
+    public void init() {
+        this.ioService = Mockito.mock(LocalizedIOService.class);
+        this.studentService = new StudentServiceImpl(ioService);
     }
 
-    @ParameterizedTest
-    @MethodSource("generateData")
-    public void test(String firstname, String lastname) {
+    @Test
+    public void test1() {
+        test("Иван", "Иванов");
+    }
+
+    @Test
+    public void test2() {
+        test("Иван", null);
+    }
+
+    @Test
+    public void test3() {
+        test(null, "Иванов");
+    }
+
+    @Test
+    public void test4() {
+        test(null, null);
+    }
+
+    private void test(String firstname, String lastname) {
         Mockito.when(ioService.readStringWithPromptLocalized("StudentService.input.first.name"))
                 .thenReturn(firstname);
         Mockito.when(ioService.readStringWithPromptLocalized("StudentService.input.last.name"))
@@ -34,14 +48,5 @@ public class StudentServiceTest {
         Student studentFromService = studentService.determineCurrentStudent();
         Student simpleStudent = new Student(firstname, lastname);
         Assertions.assertEquals(simpleStudent.getFullName(), studentFromService.getFullName());
-    }
-
-    private static Stream<Arguments> generateData() {
-        return Stream.of(
-                Arguments.of("Иван", "Иванов"),
-                Arguments.of("Иван", null),
-                Arguments.of(null, "Иванов"),
-                Arguments.of(null, null)
-        );
     }
 }
