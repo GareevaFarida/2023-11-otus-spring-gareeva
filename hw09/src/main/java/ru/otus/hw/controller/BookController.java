bb3ad2d213e3669495b84d53e46a68838c083f0b
Farida <Purumok6!>
2024-02-16 23:50:17 +0300

    hw09 исправление замечаний после code-review

--- hw09/src/main/java/ru/otus/hw/controller/BookController.java
+++ hw09/src/main/java/ru/otus/hw/controller/BookController.java
@@ -2,17 +2,14 @@ package ru.otus.hw.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.BookService;
@@ -47,7 +44,7 @@ public class BookController {
         return "book/edit";
     }
 
-    @PostMapping("/books/{id}")
+    @DeleteMapping("/books/{id}")
     public String deleteBook(@PathVariable("id") long id) {
         bookService.deleteById(id);
         return "redirect:/books";
@@ -64,11 +61,8 @@ public class BookController {
         return "book/edit";
     }
 
-    @RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},
-            method = {RequestMethod.POST},
-            value = "/books")
-    public String savebook(@RequestBody/*пыталась добиться передачи заполненного тела из теста, но не получилось*/
-                           @Valid
+    @PostMapping("/books")
+    public String savebook(@Valid
                            @ModelAttribute("book") BookDto book,
                            BindingResult bindingResult,
                            Model model) {
--- hw09/src/test/java/ru/otus/hw/controller/BookControllerTest.java
+++ hw09/src/test/java/ru/otus/hw/controller/BookControllerTest.java
@@ -1,14 +1,13 @@
 package ru.otus.hw.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.dto.BookDto;
@@ -17,15 +16,17 @@ import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.BookService;
 import ru.otus.hw.services.GenreService;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
@@ -56,142 +57,7 @@ public class BookControllerTest {
         books = getDbBooks(authors, genres);
     }
 
-    @DisplayName("Проверяет, что endpoint Get '/books' верно заполняет модель и возвращает верные статус и имя модели")
-    @Test
-    void getBooksTest() throws Exception {
-        prepareMockData();
-        var modelAndView = mvc.perform(get("/books"))
-                .andExpect(status().isOk())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("book/list"))
-                .matches(m -> !m.getModel().isEmpty());
-        Assertions.assertThat(modelAndView.getModel().get("books"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(books);
-    }
-
-    @DisplayName("Проверяет, что endpoint Get '/books/{id}' верно заполняет модель и возвращает верные статус и имя модели")
-    @Test
-    void getBooksIdTest() throws Exception {
-        prepareMockData();
-        var modelAndView = mvc.perform(get("/books/1"))
-                .andExpect(status().isOk())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("book/edit"))
-                .matches(m -> !m.getModel().isEmpty());
-        Assertions.assertThat(modelAndView.getModel().get("book"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(books.get(0));
-    }
-
-    @DisplayName("Проверяет, что endpoint Post '/books/{id}' возвращает верный статус")
-    @Test
-    void postBooksIdTest() throws Exception {
-        prepareMockData();
-        var modelAndView = mvc.perform(post("/books/1"))
-                .andExpect(status().isFound())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("redirect:/books"))
-                .matches(m -> m.getModel().isEmpty());
-    }
-
-    @DisplayName("Проверяет, что endpoint Get '/books/addnew' возвращает верные статус, модели и имя представления")
-    @Test
-    void getBooksAddnewTest() throws Exception {
-        prepareMockData();
-        var modelAndView = mvc.perform(get("/books/addnew"))
-                .andExpect(status().isOk())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("book/edit"))
-                .matches(m -> !m.getModel().isEmpty());
-        Assertions.assertThat(modelAndView.getModel().get("book"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .ignoringFields("author", "genre")
-                .isEqualTo(new BookDto());
-        Assertions.assertThat(modelAndView.getModel().get("authors"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(authors);
-        Assertions.assertThat(modelAndView.getModel().get("genres"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(genres);
-    }
-
-    @DisplayName("Проверяет, что endpoint Post '/books' возвращает верные статус, модели и имя представления")
-    @Test
-    void postBooksTest() throws Exception {
-        prepareMockData();
-        var expectedBook = books.get(0);
-        var modelAndView = mvc.perform(
-                post("/books")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        //я навешала кучу аннотаций в контроллере, чтобы передать в метод заполненную
-                        //  модель книги, но это все равно не помогло, она передается как пустая.
-                        // Ниже закомментирована проверка, которую хотела выполнить.
-                        .content(mapper.writeValueAsString(expectedBook).getBytes(StandardCharsets.UTF_8)))
-                .andExpect(status().isOk())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("book/edit"))
-                .matches(m -> !m.getModel().isEmpty());
-        //хотела добиться выполнения этого условия, но не смогла
-        //        Assertions.assertThat(modelAndView.getModel().get("book"))
-        //                .isNotNull()
-        //                .usingRecursiveComparison()
-        //                .isEqualTo(expectedBook);
-        Assertions.assertThat(modelAndView.getModel().get("authors"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(authors);
-        Assertions.assertThat(modelAndView.getModel().get("genres"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(genres);
-    }
-
-    @DisplayName("Проверяет, что Get '/books/{id}/delete' возвращает верные статус, модели и имя представления")
-    @Test
-    public void getBooksIdDelete() throws Exception {
-        prepareMockData();
-        var modelAndView = mvc.perform(get("/books/1/delete"))
-                .andExpect(status().isOk())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("book/delete"))
-                .matches(m -> !m.getModel().isEmpty());
-        Assertions.assertThat(modelAndView.getModel().get("book"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(books.get(0));
-        Assertions.assertThat(modelAndView.getModel().get("authors"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(Optional.of(authors.get(0)));
-        Assertions.assertThat(modelAndView.getModel().get("genres"))
-                .isNotNull()
-                .usingRecursiveComparison()
-                .isEqualTo(Optional.of(genres.get(0)));
-    }
-
-    @DisplayName("Проверяет, что в случае поиска по отсутствующему ИД перекинет на страницу ошибки")
-    @Test
-    public void redirectToErrorPage() throws Exception {
-        given(bookService.findBookById(1L)).willReturn(Optional.empty());
-        var modelAndView = mvc.perform(get("/books/1/delete"))
-                .andExpect(status().isBadRequest())
-                .andReturn().getModelAndView();
-        Assertions.assertThat(modelAndView)
-                .matches(m -> m.getViewName().equals("error"));
-    }
-
+    @BeforeEach
     private void prepareMockData() {
         given(bookService.findBookById(1L)).willReturn(Optional.of(books.get(0)));
         given(authorService.findById(1L)).willReturn(Optional.of(authors.get(0)));
@@ -199,6 +65,73 @@ public class BookControllerTest {
         given(bookService.findAll()).willReturn(books);
         given(authorService.findAll()).willReturn(authors);
         given(genreService.findAll()).willReturn(genres);
+    }
+
+    @DisplayName("Проверяет, что endpoint Get '/books' верно заполняет модель и возвращает верные статус и имя модели")
+    @Test
+    void getBooksTest() throws Exception {
+        prepareMockData();
+        mvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/list"))
+                .andExpect(model().attribute("books", books))
+        ;
+    }
+
+    @DisplayName("Проверяет, что endpoint Get '/books/{id}' верно заполняет модель и возвращает верные статус и имя модели")
+    @Test
+    void getBooksIdTest() throws Exception {
+        mvc.perform(get("/books/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/edit"))
+                .andExpect(model().attribute("book", books.get(0)));
+    }
+
+    @DisplayName("Проверяет, что endpoint Post '/books/{id}' перенеправляет на другой endpoint")
+    @Test
+    void postBooksIdTest() throws Exception {
+        mvc.perform(delete("/books/1"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @DisplayName("Проверяет, что endpoint Get '/books/addnew' возвращает верные статус, модели и имя представления")
+    @Test
+    void getBooksAddnewTest() throws Exception {
+        mvc.perform(get("/books/addnew"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/edit"))
+                .andExpect(model().attribute("authors", authors))
+                .andExpect(model().attribute("genres", genres));
+    }
+
+    @DisplayName("Проверяет, что endpoint Post '/books' возвращает верные статус, модели и имя представления")
+    @Test
+    void postBooksTest() throws Exception {
+        mvc.perform(
+                post("/books"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/edit"))
+                .andExpect(model().attribute("authors", authors))
+                .andExpect(model().attribute("genres", genres));
+    }
+
+    @DisplayName("Проверяет, что Get '/books/{id}/delete' возвращает верные статус, модели и имя представления")
+    @Test
+    public void getBooksIdDelete() throws Exception {
+        mvc.perform(get("/books/1/delete"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/delete"))
+                .andExpect(model().attribute("authors", Optional.of(authors.get(0))))
+                .andExpect(model().attribute("genres", Optional.of(genres.get(0))));
+    }
+
+    @DisplayName("Проверяет, что в случае поиска по отсутствующему ИД перекинет на страницу ошибки")
+    @Test
+    public void redirectToErrorPage() throws Exception {
+        given(bookService.findBookById(1L)).willReturn(Optional.empty());
+        mvc.perform(get("/books/1/delete"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("error"));
     }
 
     private static List<AuthorDto> getDbAuthors() {
