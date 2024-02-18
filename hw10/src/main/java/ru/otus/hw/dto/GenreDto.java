c58d0321c535ab664a928b8b7e4e4c80e83d69c8
Farida <Purumok6!>
2024-02-18 14:56:59 +0300

    hw10 coped hw09

---
+++ hw10/src/main/java/ru/otus/hw/dto/GenreDto.java
@@ -0,0 +1,24 @@
+package ru.otus.hw.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GenreDto {
+
+    private long id;
+
+    @NotBlank(message = "Genre's name shouldn't be empty")
+    @Size(min = 2, max = 100, message = "expected size should be more than 2 chars")
+    @Size(max = 20, message = "expected size should be less than 20 chars")
+    private String name;
+}
